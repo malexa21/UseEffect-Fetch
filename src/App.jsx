f7f90b2c22a1f1aaec93cc911b40efb2545263d1
malexa21 <aaliroalexandra@gmail.com>
2023-12-02 18:06:02 +0200

    I made further changes

--- src/App.jsx
+++ src/App.jsx
@@ -1,15 +1,15 @@
-import { useState, useEffect } from "react";
+import { useState } from "react";
+import { useEffect } from "react";
 import "./App.css";
 
 const URL =
   "https://ensemble-api.open-meteo.com/v1/ensemble?latitude=52.52&longitude=13.41&hourly=temperature_2m&models=icon_seamless";
 
 function App() {
-  const [isLoading, setIsLoading] = useState(false);
-  const [meteoData, setMeteoData] = useState(undefined);
+  const [isLoading, setIsLoading] = useState(true);
+  const [tempData, setTempData] = useState(undefined);
 
   useEffect(() => {
-    setIsLoading(true);
 
     fetch(URL, { method: "GET" })
       .then(
@@ -22,6 +22,7 @@ function App() {
       .then((data) => {
         setIsLoading(false);
 
+        //destructurarea datelor necesare
         const { 
           temperature_2m,
           temperature_2m_member01 ,
@@ -30,12 +31,13 @@ function App() {
           temperature_2m_member04 , 
           time} = data.hourly;
 
-        setMeteoData({
+        //atribuim chei datelor destructurate
+        setTempData({
           standard: temperature_2m,
-          day1: temperature_2m_member01,
-          day2: temperature_2m_member02,
-          day3: temperature_2m_member03,
-          day4: temperature_2m_member04,
+          temp1: temperature_2m_member01,
+          temp2: temperature_2m_member02,
+          temp3: temperature_2m_member03,
+          temp4: temperature_2m_member04,
           time: time,
         });
 
@@ -55,7 +57,7 @@ function App() {
   return (
     <div>
       <h1>Fetch</h1>
-      {meteoData && (
+      {tempData && (
           <table border="1px solid black">
             <thead>
               <tr>
@@ -68,14 +70,14 @@ function App() {
               </tr>
             </thead>
             <tbody>
-              {meteoData.standard.map((_, i) => (
+              {tempData.standard.map(( time , i) => (
                 <tr key={i}>
-                  <td>{meteoData.time[i]}</td>
-                  <td>{meteoData.standard[i]}</td>
-                  <td>{meteoData.day1[i]}</td>
-                  <td>{meteoData.day2[i]}</td>
-                  <td>{meteoData.day3[i]}</td>
-                  <td>{meteoData.day4[i]}</td>
+                  <td>{time}</td>
+                  <td>{tempData.standard[i]}</td>
+                  <td>{tempData.temp1[i]}</td>
+                  <td>{tempData.temp2[i]}</td>
+                  <td>{tempData.temp3[i]}</td>
+                  <td>{tempData.temp4[i]}</td>
                 </tr>
             ))}
             </tbody>
